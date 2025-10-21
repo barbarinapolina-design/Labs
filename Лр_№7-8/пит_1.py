70eb54ad6193f248f1eba9e63868feb8fa47a0cb
Lina <barbarinapolina@gmail.com>
2025-10-21 19:58:48 +0300

    Add files via upload

--- Лр_№7-8/пит_1.py
+++ Лр_№7-8/пит_1.py
@@ -1,3 +1,32 @@
-a=str(input())
-for i in range(3,len(a)):
-    print(a[i])
+import random
+a = [random.randint(-52, 65) for _ in range(20)]
+print("Массив:", a)
+
+max = a[0]
+maxi = 0
+for i in range(20):
+    if a[i] > max:
+        max = a[i]
+        maxi = i
+print("Самый большой: ", max, "на позиции ", maxi + 1)
+
+min = 1000
+mini = -1
+for i in range(20):
+    if a[i] > 0 and a[i] < min:
+        min = a[i]
+        mini = i
+if min != 1000:
+    print("Самый маленький положительный: ", min, "на позиции ", mini + 1)
+else:
+    print("Положительных нет")
+
+last5 = -1
+for i in range(20):
+    if a[i] % 5 == 0:
+        last5 = i
+if last5 != -1:
+    print("Последний кратный 5 на позиции: ", last5 + 1)
+else:
+    print("Кратных 5 нет")
+
