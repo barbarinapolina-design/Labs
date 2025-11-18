a1c192154116b3c8fe1cb7970695dabe3ba4cb14
Lina <barbarinapolina@gmail.com>
2025-11-18 11:24:08 +0300

    Add files via upload

---
+++ Лр_11/L4_2task1.pas
@@ -0,0 +1,16 @@
+﻿uses GraphABC;
+begin
+circle(50,50,20);
+floodfill(50,50,clred);
+moveto(70,50);
+lineto(120,20);
+lineto(170,50);
+lineto(70,50);
+lineto(120,80);
+lineto(170,50);
+floodfill(120,30,clblue);
+floodfill(100,60,clgreen);
+moveto(160,50);
+circle(190,50,20);
+floodfill(190,50,clyellow)
+end.
---
+++ Лр_11/L4_2task7.pas
@@ -0,0 +1,30 @@
+﻿uses GraphABC;
+var x, y, a, b: integer;
+begin
+  x:=40;
+  a:=300;
+  b:=300;
+  y:=a;
+  while y > 100 do
+  begin
+    setpencolor(clwhite);
+    setbrushcolor(clwhite);
+    rectangle(x-1, y-1, x+20, y+20);
+    setpencolor(clblack);
+    setbrushcolor(clblack);
+    rectangle(x, y, x+19, y+19);
+    x:= x + 1;
+    y:= y - 1;
+  end;
+  while y < b do
+  begin
+    setpencolor(clwhite);
+    setbrushcolor(clwhite);
+    rectangle(x-1, y-1, x+20, y+20);
+    setpencolor(clblack);
+    setbrushcolor(clblack);
+    rectangle(x, y, x+19, y+19);
+    x:= x + 1;
+    y:= y + 1;
+  end;
+end.
